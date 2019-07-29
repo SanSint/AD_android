02a3640c081da9df157b23a828ea43595dfb07d1
sansint <sansintsint@gmail.com>
2019-07-29 19:31:12 +0800

    Git Test

--- app/src/main/java/com/san/logicuniversity_ad/MainActivity.java
+++ app/src/main/java/com/san/logicuniversity_ad/MainActivity.java
@@ -2,7 +2,9 @@ package com.san.logicuniversity_ad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("TAG", "Testing Git");
     }
 }
