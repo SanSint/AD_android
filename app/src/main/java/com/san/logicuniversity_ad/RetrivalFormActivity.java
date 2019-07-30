1b747233fe9298f23a67411064026d723b99aef4
sansint <sansintsint@gmail.com>
2019-07-30 10:44:12 +0800

    some layout

---
+++ app/src/main/java/com/san/logicuniversity_ad/RetrivalFormActivity.java
@@ -0,0 +1,14 @@
+package com.san.logicuniversity_ad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetrivalFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrival_form);
+    }
+}
---
+++ app/src/main/java/com/san/logicuniversity_ad/StoreClerk_menu_Activity.java
@@ -0,0 +1,14 @@
+package com.san.logicuniversity_ad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreClerk_menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_clerk_menu_);
+    }
+}
