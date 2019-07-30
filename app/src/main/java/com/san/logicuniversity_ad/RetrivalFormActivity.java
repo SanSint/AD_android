63e3780fb17cc294c20c5923068b7d94bcad9424
sansint <sansintsint@gmail.com>
2019-07-30 13:44:55 +0800

    flow

--- app/src/main/java/com/san/logicuniversity_ad/RetrivalFormActivity.java
+++ app/src/main/java/com/san/logicuniversity_ad/RetrivalFormActivity.java
@@ -2,13 +2,21 @@ package com.san.logicuniversity_ad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class RetrivalFormActivity extends AppCompatActivity {
+public class RetrivalFormActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrival_form);
+
     }
+
+
 }
--- app/src/main/java/com/san/logicuniversity_ad/StoreClerk_menu_Activity.java
+++ app/src/main/java/com/san/logicuniversity_ad/StoreClerk_menu_Activity.java
@@ -2,13 +2,27 @@ package com.san.logicuniversity_ad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class StoreClerk_menu_Activity extends AppCompatActivity {
+public class StoreClerk_menu_Activity extends AppCompatActivity
+        implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_clerk_menu_);
+
+        ImageButton btn=findViewById(R.id.retivial_from_btn);
+
+        btn.setOnClickListener(this);
+    }
+    public void onClick(View v)
+    {
+        Intent retrieve=new Intent(this,RetrivalFormActivity.class);
+        startActivity(retrieve);
+
     }
 }
