e5360e7cc9986ba925fc3774a5409d6e08fde5dd
sansint <sansintsint@gmail.com>
2019-07-31 16:29:57 +0800

    updated

--- app/src/main/java/com/san/logicuniversity_ad/Store_RetrivalForm.java
+++ app/src/main/java/com/san/logicuniversity_ad/Store_RetrivalForm.java
@@ -2,16 +2,27 @@ package com.san.logicuniversity_ad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Store_RetrivalForm extends AppCompatActivity
-{
+    implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_retrival_form);
 
+        Button btn=findViewById(R.id.btn_submit);
+        btn.setOnClickListener(this);
+
+    }
+    public void onClick(View v)
+    {
+        Intent view=new Intent(this,Store_Success.class);
+        startActivity(view);
     }
 
 
